aa7b08b96f08f5350719102b6634586c0200e33d
nicolascolque <nicolascolque@gmail.com>
2017-09-17 20:24:40 -0300

    Conversor DecimalBinario

--- src/DecimalaBinario/Convercion.java
+++ src/DecimalaBinario/Convercion.java
@@ -17,23 +17,26 @@ public class Convercion
                         aux = numero;
                         while (aux !=0) 
                     {
-                        digito = aux % 10;
-                        if (digito != 1 && digito !=0) 
+                        digito = aux % 10;//le asigno a digito el ultimo numero 
+                        if (digito != 1 && digito !=0) //controlo si es distinito de 0 y 1
                             {
-                               esbinario = false;    
+                               esbinario = false;
+                                System.out.println("Ingresa solo un numero Binario Compuesto por ' 0 y 1' ");
+                                System.out.println("");
                             }
-                        aux = aux /10;
+                        aux = aux /10;//quitamos el ultimo numero 
                     }
                     
-                  } while (!esbinario);
+                  } while (!esbinario);//controlo que sea un numero binario
                     exp = 0;
                     decimal = 0;
-                    while (numero !=0) 
+                    while (numero !=0)// controlo que no sea un numero 0
                         {                    
-                            digito = numero % 10;
+                            digito = numero % 10;//separa el ultimo numero 
+                            //suma los numeros elevados a la potencia de 2
                             decimal= decimal + digito * (int)Math.pow(2, exp);
-                             exp++;
-                             numero = numero/10;
+                             exp++;//cada vez que pasa suma el exp + 1
+                             numero = numero/10;//quito el ultimo numero 
                         }
                 System.out.println("El Numero decimal es: " + decimal);
     }
